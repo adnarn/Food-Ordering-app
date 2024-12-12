5f687683b19746cd1dc41d70fca01649ff69eee9
Dex_Coder <adnusy2023@gmail.com>
2024-12-12 13:37:14 +0100

    Update ListItems.jsx

--- admin/src/pages/ListItems/ListItems.jsx
+++ admin/src/pages/ListItems/ListItems.jsx
@@ -7,10 +7,11 @@ const ListItems = ({theme}) => {
   const [list, setList] = useState([]);
   const listNum = list.length
   console.log(listNum)
+  const [loading, setLoading] = useState(true);
 
   const fetchList = async () => {
     try {
-      const response = await axios.get('http://localhost:8000/api/food/list');
+      const response = await axios.get('https://food-ordering-app-qi5n.onrender.com/api/food/list');
       if (response.data.success) {
         setList(response.data.data);
         console.log(response.data.data)
@@ -20,6 +21,8 @@ const ListItems = ({theme}) => {
     } catch (error) {
       // toast.error("An error occurred while fetching the list");
       console.error('Error fetching list:', error);
+    }finally {
+      setLoading(false); // Stop loading after fetching
     }
   };
 
@@ -28,7 +31,7 @@ const ListItems = ({theme}) => {
   }, []);
 
   const removeFood = async (foodId) => {
-      const response = await axios.post('http://localhost:8000/api/food/remove',{id:foodId});
+      const response = await axios.post('https://food-ordering-app-qi5n.onrender.com/api/food/remove',{id:foodId});
       await fetchList();
       if(response.data.success) {
         toast.success("Food Removed")
@@ -43,7 +46,9 @@ const ListItems = ({theme}) => {
         <h3 className='text-header'>All Foods List</h3>
         <div className="list-table">
 
-          { listNum!==0?(
+          {loading?(
+            <p>Loading ...</p>
+          ) : listNum!==0?(
             <>
             <div className="list-table-format title">
                 <b>Image</b>
@@ -55,7 +60,7 @@ const ListItems = ({theme}) => {
             {list.map((item, index) =>{
             return(
             <div className="list-table-format">
-            <img src={"http://localhost:8000/uploads/"+item.imgurl} alt="" />
+            <img src={"https://food-ordering-app-qi5n.onrender.com/uploads/"+item.imgurl} alt="" />
             <p>{item.name}</p>
             <p>${item.price}</p>
             <p className='X' onClick={()=>removeFood(item._id)}>X</p>
